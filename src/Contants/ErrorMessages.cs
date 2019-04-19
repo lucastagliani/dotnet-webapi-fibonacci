2e41d998457238f16538ba7ade3ae469f4813976
Lucas Tagliani <lucas.tagliani@outlook.com>
2019-04-18 22:39:12 -0300

    refact: create error messages constants

---
+++ src/Contants/ErrorMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_webapi_fibonacci.Contants
+{
+    public static class ErrorMessages
+    {
+        public static string MUST_BE_GREATER_THAN_ZERO = "The limit must be greater than one.";
+        public static string MUST_BE_LESS_THAN_ONE_THOUSAND = "The limit must be less than 1000.";
+    }
+}
