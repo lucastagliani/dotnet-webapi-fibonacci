ba1b460d76872651f1e7213390f622f398c725d5
Lucas Tagliani <lucas.tagliani@outlook.com>
2019-04-18 22:35:25 -0300

    refac: remove unused code

--- src/Controllers/FibonacciController.cs
+++ src/Controllers/FibonacciController.cs
@@ -31,23 +31,5 @@ namespace dotnet_webapi_fibonacci.Controllers
 
             return fibonacciService.GenerateFibonacci(limit);
         }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
