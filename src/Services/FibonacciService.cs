810b6d0ad925632040b09363e0314d5fa3b21dc8
Lucas Tagliani <lucas.tagliani@outlook.com>
2019-04-18 23:03:40 -0300

    feat: test new fibonacci sequence, implementation and refactoring

--- src/Services/FibonacciService.cs
+++ src/Services/FibonacciService.cs
@@ -11,19 +11,53 @@ namespace dotnet_webapi_fibonacci.Services
         private int MINIMUM_LIMIT = 0;
         private int MAXIMUM_LIMIT = 1000;
 
-        public int[] GenerateFibonacci(int limit)
+        public int[] GenerateFibonacciSequenceUntil(int limit)
+        {
+            ValidateLimit(limit);
+            return CalculateFibonacciSequence(limit);
+        }
+
+        private void ValidateLimit(int limit)
         {
             if (limit <= MINIMUM_LIMIT)
             {
                 throw new ArgumentException(ErrorMessages.MUST_BE_GREATER_THAN_ZERO);
             }
 
-            if (limit > MAXIMUM_LIMIT)
+            if (MAXIMUM_LIMIT < limit)
             {
                 throw new ArgumentException(ErrorMessages.MUST_BE_LESS_THAN_ONE_THOUSAND);
             }
+        }
 
-            return new int[] { 1, 1, 2, 3, 5, 8 };
+        private int[] CalculateFibonacciSequence(int limit)
+        {
+            List<int> fibonacciListResult = new List<int>() { 0, 1 };
+
+            int penultimateNumber = 0;
+            int lastNumber = 1;
+            int fibonacciNumber = CalculateFibonacciNumber(penultimateNumber, lastNumber);
+
+            while (IsFibonacciNumberLessOrEqualLimit(fibonacciNumber, limit))
+            {
+                fibonacciListResult.Add(fibonacciNumber);
+
+                penultimateNumber = lastNumber;
+                lastNumber = fibonacciNumber;
+                fibonacciNumber = CalculateFibonacciNumber(penultimateNumber, lastNumber);
+            }
+
+            return fibonacciListResult.ToArray();
+        }
+
+        private int CalculateFibonacciNumber(int penultimateNumber, int lastNumber)
+        {
+            return penultimateNumber + lastNumber;
+        }
+
+        private bool IsFibonacciNumberLessOrEqualLimit(int fibonacciNumber, int limit)
+        {
+            return fibonacciNumber <= limit;
         }
     }
 }
