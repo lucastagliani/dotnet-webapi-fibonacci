9bdbece0c8be22301f058991a3b592b99ed0f873
Lucas Tagliani <lucas.tagliani@outlook.com>
2019-04-21 01:04:02 -0300

    feat: the minimum for fibonacci sequence length test passing

--- src/Services/FibonacciService.cs
+++ src/Services/FibonacciService.cs
@@ -61,7 +61,7 @@ namespace dotnet_webapi_fibonacci.Services
 
         public int[] GetFibonacciSequenceWithLength(int length)
         {
-            throw new NotImplementedException();
+            return new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
         }
     }
 }
