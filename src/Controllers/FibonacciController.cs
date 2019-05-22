7821398a89a42735c92bb8351bf2a83111ec3f35
Lucas Tagliani <lucas.tagliani@zenvia.com>
2019-05-21 22:57:07 -0300

    refact: remove required from paramns and add connectionString to mongo prod

--- src/Controllers/FibonacciController.cs
+++ src/Controllers/FibonacciController.cs
@@ -15,9 +15,8 @@ namespace dotnet_webapi_fibonacci.Controllers
     {
         private readonly string ALGORITHM_NAME_LENGTH = "length";
         private readonly string ALGORITHM_NAME_UNTIL = "until";
-
-        private readonly IFibonacciService _service;
         private readonly ILogRepository _log;
+        private readonly IFibonacciService _service;
 
         public FibonacciController(IFibonacciService fibonacciService, ILogRepository logRepository)
         {
@@ -25,7 +24,7 @@ namespace dotnet_webapi_fibonacci.Controllers
             _log = logRepository;
         }
         // GET api/fibonacci/until/100
-        [HttpGet("{limit}")]
+        [HttpGet("{limit?}")]
         public ActionResult<FibonacciResult> Until(int limit = 100)
         {
             FibonacciResult fibonacciResult;
@@ -33,25 +32,25 @@ namespace dotnet_webapi_fibonacci.Controllers
             try
             {
                 fibonacciResult = new FibonacciResult(_service.GetFibonacciSequenceUntil(limit));
-                _log.Create(new Log(ALGORITHM_NAME_UNTIL, "", fibonacciResult));
+                _log.Create(new Log(ALGORITHM_NAME_UNTIL, limit, fibonacciResult));
                 return Ok(fibonacciResult);
             }
             catch (ArgumentException ex)
             {
                 fibonacciResult = new FibonacciResult(ex.Message);
-                _log.Create(new Log(ALGORITHM_NAME_UNTIL, "", fibonacciResult));
+                _log.Create(new Log(ALGORITHM_NAME_UNTIL, limit, fibonacciResult));
                 return BadRequest(fibonacciResult);
             }
             catch (Exception ex)
             {
                 fibonacciResult = new FibonacciResult(ex.Message);
-                _log.Create(new Log(ALGORITHM_NAME_UNTIL, "", fibonacciResult));
+                _log.Create(new Log(ALGORITHM_NAME_UNTIL, limit, fibonacciResult));
                 return StatusCode(500, fibonacciResult);
             }
         }
 
         // GET api/fibonacci/length/10
-        [HttpGet("{length}")]
+        [HttpGet("{length?}")]
         public ActionResult<FibonacciResult> Length(int length = 100)
         {
             FibonacciResult fibonacciResult;
@@ -59,19 +58,19 @@ namespace dotnet_webapi_fibonacci.Controllers
             try
             {
                 fibonacciResult = new FibonacciResult(_service.GetFibonacciSequenceWithLength(length));
-                _log.Create(new Log(ALGORITHM_NAME_LENGTH, "", fibonacciResult));
+                _log.Create(new Log(ALGORITHM_NAME_LENGTH, length, fibonacciResult));
                 return Ok(fibonacciResult);
             }
             catch (ArgumentException ex)
             {
                 fibonacciResult = new FibonacciResult(ex.Message);
-                _log.Create(new Log(ALGORITHM_NAME_LENGTH, "", fibonacciResult));
+                _log.Create(new Log(ALGORITHM_NAME_LENGTH, length, fibonacciResult));
                 return BadRequest(fibonacciResult);
             }
             catch (Exception ex)
             {
                 fibonacciResult = new FibonacciResult(ex.Message);
-                _log.Create(new Log(ALGORITHM_NAME_LENGTH, "", fibonacciResult));
+                _log.Create(new Log(ALGORITHM_NAME_LENGTH, length, fibonacciResult));
                 return StatusCode(500, fibonacciResult);
             }
         }
